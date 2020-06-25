ec1f8f177f11cfc9c4bd89188cfe3f003a70f25b
shubham <shubham17.sm@gmail.com>
2020-06-26 02:55:54 +0530

    added multiple urls for scraping logic

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -9,42 +9,46 @@ from bs4 import BeautifulSoup
 from celery import shared_task
 from .models import News
 
+from fake_useragent import UserAgent
+
 
 @shared_task
 def news_task():
     print('Collecting news article..')
-    url = "https://timesofindia.indiatimes.com/rssfeeds/1221656.cms"
+    urls = ['https://timesofindia.indiatimes.com/rssfeeds/1221656.cms',
+            'https://www.hindustantimes.com/rss/topnews/rssfeed.xml']
 
-    r = requests.get(url)
-    htmlcontent = r.content
-    # print(htmlcontent)
+    user_agent = UserAgent()
 
-    soup = BeautifulSoup(htmlcontent, "xml")
-    # print(soup.prettify)
+    for url in urls:
+        r = requests.get(url, headers={"user-agent": user_agent.chrome})
+        htmlcontent = r.content
+        # print(htmlcontent)
 
+        soup = BeautifulSoup(htmlcontent, "xml")
+        # print(soup.prettify)
 
-    rows = soup.find_all('item')
+        rows = soup.find_all('item')
 
-    for row in rows:
-        title = row.find('title').get_text()
-        description = row.find('description').get_text()
-        link = row.find('link').get_text()
-        guid = row.find('guid').get_text()
-        pubdate = row.find('pubDate').get_text()
+        for row in rows:
+            title = row.find('title').get_text()
+            description = row.find('description').get_text()
+            link = row.find('link').get_text()
+            guid = row.find('guid').get_text()
+            pubdate = row.find('pubDate').get_text()
 
-        print({'title': title, 'description': description, 'link': link, 'guid': guid, 'pubdate': pubdate})
+            print({'title': title, 'description': description,
+                   'link': link, 'guid': guid, 'pubdate': pubdate})
 
+            News.objects.create(
+                title=title,
+                description=description,
+                link=link,
+                pubdate=pubdate,
+                news_bool=False
+            )
 
-
-        News.objects.create(
-        title = title,
-        description = description,
-        link = link,
-        pubdate = pubdate,
-        news_bool = False
-        )
-
-        sleep(5)
+            sleep(5)
 
 
 news_task()
